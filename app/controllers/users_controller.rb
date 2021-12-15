1f29af9836c7a7ef559070c925d0d798910f23b4
Haewon Jeon <haewonito@gmail.com>
2021-12-15 15:02:23 -0700

    user story 1 and 2 complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def index
   end
-  
+
   def show
     @user = User.find(params[:id])
   end
@@ -13,8 +13,30 @@ class UsersController < ApplicationController
   def create
     email = params[:email]
     name = params[:name]
-    user = User.create(name: name, email: email)
+    password = params[:password]
+    password_confirmation = params[:password_confirmation]
 
-    redirect_to "/users/#{user.id}"
+    user = User.create(name: name, email: email, password: password, password_confirmation: password_confirmation)
+
+    if user.errors.any?
+      redirect_to "/users"
+      error_message = user.errors.full_messages.first
+      flash[:error] = error_message
+    elsif user.save
+      redirect_to "/users/#{user.id}"
+    end
+
+
+    #
+    # if password != password_confirmation
+    #   flash[:alert] = "The passwords don't match. Try again."
+    #   redirect_to register_path
+    # elsif name = nil
+    #
+    #   redirect_to register_path
+    # else
+      # user = User.create(name: name, email: email, password: password, password_confirmation: password_confirmation)
+      # redirect_to "/users/#{user.id}"
+    # end
   end
 end
